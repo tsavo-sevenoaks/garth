7c6cd443836be8fb178a4e0472458407ab848cad
tsavo-sevenoaks <bruce.woodley.tsavo@gmail.com>
2016-08-20 15:41:52 +0100

    Add files via upload

---
+++ #My_Jupyter_Startup_script.bat
@@ -0,0 +1,10 @@
+REM Initizlisation script to start up the Python3 based Jupyter Notebook
+
+
+echo START: Initizlisation script to start up the Python3 based Jupyter Notebook
+
+cd C:\Users\bruce\OneDrive\Udacity_Intro_to_Data_Analysis 
+REM ipython3 notebook
+jupyter notebook
+
+echo STOP: Initizlisation script to start up the Python3 based Jupyter Notebook
